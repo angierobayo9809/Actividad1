bf9f0bc04eabfc17936aad60f78fa9d1519658ed
angierobayo09 <arobayo@ibiscreativa.com>
2023-07-16 10:11:11 -0500

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+function validacionDatos(){
+    let nombre = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let telefono = document.getElementById("phone").value;
+    let comentario = document.getElementById("comment").value;
+
+}
